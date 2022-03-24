5d200d84058884a9f9cbc86834757476e9861414
kevanteaniah <kevanteaniah@gmail.com>
2022-03-24 19:16:18 -0400

    Add index && create

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as postsRouter } from './routes/posts.js'
 
 import('./config/database.js')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/posts', postsRouter)      
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: "Not found" })
