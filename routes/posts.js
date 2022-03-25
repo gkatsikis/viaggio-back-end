229745c59ef77e020e92c0ff550564ecb700f613
kevanteaniah <kevanteaniah@gmail.com>
2022-03-25 18:52:46 -0400

    Finish comment schema

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,11 @@ router.get('/:id', checkAuth, postsCtrl.show)
 router.put('/:id', checkAuth, postsCtrl.update)
 router.delete('/:id', checkAuth, postsCtrl.delete)
 
+//comments
+router.post('/:id/comments', checkAuth, postsCtrl.createComment)
+router.delete('/:postId/comments/:commentId', checkAuth, postsCtrl.deleteComment)
+router.put('/:postId/comments/:commentId', checkAuth, postsCtrl.updateComment)
+
 
 export {
   router
