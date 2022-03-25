cb6c02d0c77bd7f0aaed7190879d284d5711e686
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-25 10:32:51 -0400

    create function bug solved

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,20 +1,21 @@
 import { Post } from '../models/post.js'
 
 function index (req, res) {
+  console.log('INDEX')
   Post.find({})
-  // Add this line
   .populate('owner')
   .then(posts => {
     res.json(posts)
   })
   .catch(err => {
+    console.log('ERROR', err)
     res.json(err)
   })
 }
 
 function create(req, res) {
-  // Add this line
-  req.body.owner = req.user.profile
+  console.log('CREATE')
+  // req.body.owner = req.user.profile
   Post.create(req.body)
   .then(post => res.json(post))        //check to create post
   .catch(err => res.json(err))
