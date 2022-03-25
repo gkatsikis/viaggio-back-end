21333bd38bd5168a7010e83b0daf3621ff0c5826
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-25 18:20:23 -0400

    add delete route and function

--- routes/posts.js
+++ routes/posts.js
@@ -10,6 +10,7 @@ router.get('/', postsCtrl.index)
 router.post('/',  checkAuth, postsCtrl.create)
 router.get('/:id', checkAuth, postsCtrl.show)
 router.put('/:id', checkAuth, postsCtrl.update)
+router.delete('/:id', checkAuth, postsCtrl.delete)
 
 
 export {
