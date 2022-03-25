80d3fe1af27bf458264cc11f4bc59fb62bcdf2ee
zuri227 <zuri22780@gmail.com>
2022-03-25 13:08:46 -0400

    add auth to routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import * as postsCtrl from '../controllers/posts.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
+router.use(decodeUserFromToken)
+
 router.get('/',  postsCtrl.index)
 router.post('/',  postsCtrl.create)
+router.put('/:id', checkAuth, postCtrl.update)
+router.delete('/:id', checkAuth, postCtrl.delete)
+router.get('/:id', checkAuth, postCtrl.show)
 
 export {
   router
