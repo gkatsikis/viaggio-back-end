2c277b729c261a945c3a41c9fdc31ca2f760de2c
zuri227 <zuri22780@gmail.com>
2022-03-25 13:12:18 -0400

    fix index and control routes

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,8 @@ const router = Router()
 
 router.use(decodeUserFromToken)
 
-router.get('/',  postsCtrl.index)
-router.post('/',  postsCtrl.create)
+router.get('/',  checkAuth, postsCtrl.index)
+router.post('/',  checkAuth, postsCtrl.create)
 router.put('/:id', checkAuth, postCtrl.update)
 router.delete('/:id', checkAuth, postCtrl.delete)
 router.get('/:id', checkAuth, postCtrl.show)
