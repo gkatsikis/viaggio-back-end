a9632dec52c3a5bd776657d35a94f10de2f127de
gkatsikis <geo.bjj.kat@gmail.com>
2022-03-30 16:30:46 -0400

    stub controller function for destination create

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -2,6 +2,14 @@
 
 function create (req, res) {
   console.log('DOES IT WOOOOORK????')
+  Location.create(req.body)
+  .then(location => {
+    res.json(location)
+    .catch(err => {
+      console.log(err)
+      res.json(err)
+    })
+  })
 }
 
 export {
